ae2b12a61a7bb4a93756caa13d9b3162da9cee70
MoriartyJam <jamesmoriartytrading2018@gmail.com>
2023-02-09 14:18:19 +0200

    Create download form

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import Union
+
+from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+
+@app.post("/")
+async def root(file: UploadFile = File(...)):
+    return {"file_name": file.filename}
