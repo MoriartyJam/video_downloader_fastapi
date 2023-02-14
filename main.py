fe8a6f818ab44398796714e36108a6533106c5e4
MoriartyJam <jamesmoriartytrading2018@gmail.com>
2023-02-14 09:17:16 +0200

    Multiple picture loading function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import shutil
 from typing import Union
+from typing import List
 
 from fastapi import FastAPI, UploadFile, File
 
@@ -11,3 +12,12 @@ async def root(file: UploadFile = File(...)):
     with open(f'{file.filename}', "wb") as butter:
         shutil.copyfileobj(file.file, butter)
     return {"file_name": file.filename}
+
+
+@app.post("/img")
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(f'{img.filename}', "wb") as butter:
+            shutil.copyfileobj(img.file, butter)
+    return {"file_name": "Good"}
+
